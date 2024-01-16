eb3872beb41878a66d847c1ebdcbbd5da67169da
mariamtamerr <memariam5@outlook.com>
2024-01-16 18:01:34 +0200

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Ultimate Code Review',
+  title: 'Ultimatecodereview - Cystorm.Solutions',
   description: 'Ultimate code review',
 }
 
