c015e1990b418030e3422cee2a782aa76b14eaab
mariamtamerr <memariam5@outlook.com>
2024-01-17 20:36:09 +0200

    ..

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -70,31 +70,8 @@ const handleCategoryChange4 = (category4) => {
               </div>
 
 
-              {/* <div className="relative inline-block text-left">
-              <div>
-                <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                  Options
-                  <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                  </svg>
-                </button>
-              </div>
 
-              
-              <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                <div className="py-1" role="none">
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-                  <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-                  <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-                  <form method="POST" action="#" role="none">
-                    <button type="submit" className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-                  </form>
-                </div>
-              </div>
-            </div> */}
-
-
-            {/* <div className="bg-white  mx-auto">
+            <div className="bg-white  mx-auto">
             <label className="text-blue-600  text-center mb-5 font-medium">Web Development</label>
                <select className="bg-white ml-2 flex px-4 py-3 border border-blue-600 rounded-xl" name="category1" value={category1} onChange={event => handleCategoryChange1(event.target.value)} required>
                   <option id="0" >yes</option>
@@ -110,27 +87,25 @@ const handleCategoryChange4 = (category4) => {
                   <option id="4" >Team</option>
                   <option id="5" >Enterprise</option>
               </select>
-            </div> */}
+            </div>
 
-
-            <div className="bg-white flex mt-7 sm:gap-10 gap-2 sm:mb-10 ">
-              <div>
+{/* ---------------------------------------------- */}
+            {/* <div className="bg-white flex mt-7 sm:gap-10 gap-2 sm:mb-10 ">
+              
                 <label className="text-blue-600  mb-5 font-medium">Web Development</label>
-              </div>
-              <div>
+              
                 <select className="bg-white px-4 py-2 border border-blue-600 rounded-xl" name="category1" value={category1} onChange={event => handleCategoryChange1(event.target.value)} required>
                   <option id="0" >yes</option>
                   <option id="1" >no</option>
               </select>
-              </div>
-              <div>
-                <select className="bg-white px-4 py-3 border border-blue-600 rounded-xl" name="category2" value={category2} onChange={event => handleCategoryChange2(event.target.value)}>
+             
+                <select className="bg-white px-4 py-3 border border-blue-600 rounded-xl" name="category2" value={category2} onChange={event => handleCategoryChange2(event.target.value)} >
                   <option id="2" >None</option>
                   <option id="3" >Startup</option>
                   <option id="4" >Team</option>
                   <option id="5" >Enterprise</option>
               </select>
-              </div>
+              
             </div>
             <br/>
 
@@ -146,13 +121,13 @@ const handleCategoryChange4 = (category4) => {
                   <option id="9" >Free</option>
                   <option id="10" >Enterprise</option>
               </select>
-            </div>
+            </div> */}
+
+{/* -------------------------------------------------------------------------------------- */}
 
 
 
-
-
-            {/* <div className="bg-white mx-auto">
+            <div className="bg-white mx-auto">
             <label className="text-blue-600 text-center font-medium">Cyber Security</label>
                <select className="bg-white ml-2 flex px-4 py-3 border border-blue-600 rounded-xl" name="category3" value={category3} onChange={event => handleCategoryChange3(event.target.value)} required>
                   <option id="6" >yes</option>
@@ -168,7 +143,7 @@ const handleCategoryChange4 = (category4) => {
                   <option id="10" >Enterprise</option>
               </select>
             </div>
-    */}
+   
 
             
 
